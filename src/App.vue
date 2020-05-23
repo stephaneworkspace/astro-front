5bbe60a0a05f8ee01183842091995d0db5d79fef
stephaneworkspace <s.bressani@bluewin.ch>
2020-05-23 20:31:54 +0200

    fix orth

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@
         target="_blank"
         rel="noopener"
         >code source</a
-      >. Il est sous liscence creative common. En revanche les ephémérides sont
-      sous liscence GPL3. Ils se connectent à mon source cité plus haut via un
-      autre source ici qui fait le pont entre la libraiaire C (swissephem
+      >. Il est sous licence creative common. En revanche les éphémérides sont
+      sous liscence GPL3. Ils se connectent à mon code source cité plus haut via
+      un autre code source qui fait le pont entre la libraiaire C (swissephem
       d'astrodientz
       <a href="astro.com" target="_blank" rel="noopener">astro.com</a>) et ce
       <a
