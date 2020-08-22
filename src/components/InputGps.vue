852481f9e870731afb7d53fa746914a41610a02d
stephaneworkspace <s.bressani@bluewin.ch>
2020-08-22 02:27:30 +0200

    begining of the version 0.2

--- src/components/InputGps.vue
+++ src/components/InputGps.vue
@@ -168,7 +168,7 @@ export default class InputData extends Vue {
       "Content-Type": "application/x-www-form-urlencoded"
     };
     axios
-      .get(this.api + "filter-city/" + name, config)
+      .get(this.api + "filter-city-2/" + name, config)
       .then(res => {
         this.list = res.data;
         //this.$forceUpdate();
