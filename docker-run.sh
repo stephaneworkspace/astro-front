1f2290d4cc594435587d3d81417807359770ba3f
Stéphane Bressani <stephane@bressani.dev>
2022-01-25 22:00:02 +0100

    Docker

--- docker-run.sh
+++ docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it -p 8882:80 --rm -d --name docker-vue-js-app-2  vuejs-astro-front/docker-vuejs-app
+docker run -it -p 8882:8080 --rm -d --name docker-vue-js-app-2  vuejs-astro-front/docker-vuejs-app
